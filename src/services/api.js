da33acbb879434fc4ebc6c1c01bd97fb78e82908
arthuramaral29 <arthur.amaral2@fatec.sp.gov.br>
2024-06-11 05:03:11 -0300

    Troca da URL de conexão com o Back-End

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.15:3333',
+  baseURL: "https://backend-wealth-wise.onrender.com",
 });
 
 export default api;
